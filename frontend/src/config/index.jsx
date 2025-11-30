4a1b21def3287143bbaf10add24a02a9f75086e2
shivam240104 <mauryatanmay845@gmail.com>
2025-11-30 14:16:58 +0530

    Changing the backend joining chain

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const {default : axios} = require("axios")
 
-export const BaseURL = "http://localhost:9090"
+export const BaseURL = "https://go-connect-ymlq.onrender.com/"
 
 export const clientServer = axios.create({
     baseURL :BaseURL,
